3654062af84bccf5b2c0b640346fb18eb179c22b
marisa2222 <msharifi2222@gmail.com>
2023-05-22 15:11:42 +0200

    modified hotel.js in controllers

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -41,11 +41,12 @@ export const getHotel = async (req, res, next) => {
 };
 export const getHotels = async (req, res, next) => {
   const { min, max, ...others } = req.query;
+  const filter = {
+    ...others,
+    cheapestPrice: { $gt: min | 1, $lt: max || 100000 },
+  };
   try {
-    const hotels = await Hotel.find({
-      ...others,
-      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
-    }).limit(req.query.limit);
+    const hotels = await Hotel.find(filter).limit(req.query.limit);
     res.status(200).json(hotels);
   } catch (err) {
     next(err);
